edd3d23992bf7caf91ff3ab2f72e04ffb6377880
guitarliu <whuliuxiaoyong@163.com>
2023-04-27 22:03:40 +0800

    Add Main Program

---
+++ CommandLine/Class1.cs
@@ -0,0 +1,26 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System.Diagnostics;
+using System;
+using System.Security.Principal;
+using System.Windows.Interop;
+using System.Windows;
+
+namespace CommandLine
+{
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    [Autodesk.Revit.Attributes.Regeneration(Autodesk.Revit.Attributes.RegenerationOption.Manual)]
+    public class Class1 : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+
+            CommandLineWindow commandWindow = new CommandLineWindow();
+            WindowInteropHelper mainUi = new WindowInteropHelper(commandWindow);
+            mainUi.Owner = Process.GetCurrentProcess().MainWindowHandle;
+            commandWindow.Show();
+
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ CommandLine/CommandLineWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CommandLine
+{
+    /// <summary>
+    /// CommandLineWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class CommandLineWindow : Window
+    {
+        public CommandLineWindow()
+        {
+            InitializeComponent();
+        }
+        private void DragWindow(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                DragMove();
+            }
+            catch (Exception)
+            {
+                // throw;
+            }
+        }
+        private void KeyboardTextInput(object sender, TextCompositionEventArgs e)
+        {
+            try
+            {
+                Tb_Command.Text = e.Text;
+
+            }
+            catch (Exception) 
+            {
+                Tb_Command.Text = "键入命令";
+            }
+        }
+
+        private void Bt_Close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Bt_Settings_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
